c375824264eade26807d2f20c6cae8cd4e01a529
christopher mera <chris@massiveclouds.com>
2013-10-31 20:26:48 -0400

    Initialize all variables at start of script

--- filecopy.sh
+++ filecopy.sh
@@ -2,10 +2,10 @@
 FC_ROOT="/opt/massive"
 FC_DATADIR="${FC_ROOT}/Backups/files"
 FC_SOURCE=$1
-
-[ -d ${FC_DATADIR} ] && echo "$FC_DATADIR found" || mkdir -p $FC_DATADIR
-
 date=`date "+%Y-%m-%dT%H:%M:%S"`
+
+[ -d ${FC_DATADIR} ] || echo "$FC_DATADIR not found" && exit 1;
+
 rsync -aP --link-dest=${FC_DATADIR}/current ${FC_SOURCE} ${FC_DATADIR}/back-$date
 rm -f ${FC_DATADIR}/current
 ln -s back-$date ${FC_DATADIR}/current
