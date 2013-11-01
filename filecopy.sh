b362d842fa27590999138a15e61a2e4dc6619005
christopher mera <chris@massiveclouds.com>
2013-10-31 20:29:32 -0400

    Create directory structure if FC_DATADIR test fails

--- filecopy.sh
+++ filecopy.sh
@@ -4,7 +4,7 @@ FC_DATADIR="${FC_ROOT}/Backups/files"
 FC_SOURCE=$1
 date=`date "+%Y-%m-%dT%H:%M:%S"`
 
-[ -d ${FC_DATADIR} ] || echo "$FC_DATADIR not found" && exit 1;
+[ -d ${FC_DATADIR} ] || echo "* $FC_DATADIR not found" && mkdir -p ${FC_DATADIR}
 
 rsync -aP --link-dest=${FC_DATADIR}/current ${FC_SOURCE} ${FC_DATADIR}/back-$date
 rm -f ${FC_DATADIR}/current
