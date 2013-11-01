78a8186dabd80ad11ad97727b72dbd7e100b1b07
christopher mera <chris@massiveclouds.com>
2013-10-31 21:11:22 -0400

    Directly call bash, fc_post_processing func added, fc_create_new_point func added

--- filecopy.sh
+++ filecopy.sh
@@ -1,18 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 FC_ROOT="/opt/massive"
 FC_DATADIR="${FC_ROOT}/Backups/files"
 FC_SOURCE=$1
 date=`date "+%Y-%m-%dT%H:%M:%S"`
 
-function create_starting_point {
-	echo "* $FC_DATADIR not found"
-	mkdir -p ${FC_DATADIR}
-	rsync -aP ${FC_SOURCE} ${FC_DATADIR}/back-$date
+function fc_post_processing {
+	rm -f ${FC_DATADIR}/current
+	ln -s back-$date ${FC_DATADIR}/current
 	exit 0;
 }
 
-[ -d ${FC_DATADIR} ] || create_starting_point
+function fc_create_starting_point {
+	echo "* Creating $FC_DATADIR "
+	mkdir -p ${FC_DATADIR}
+	echo "* Creating first full backup "	
+	rsync -a ${FC_SOURCE} ${FC_DATADIR}/back-$date
+	fc_post_processing
+}
 
-rsync -aP --link-dest=${FC_DATADIR}/current ${FC_SOURCE} ${FC_DATADIR}/back-$date
-rm -f ${FC_DATADIR}/current
-ln -s back-$date ${FC_DATADIR}/current
+function fc_create_new_point {
+	echo "* Creating new restore point"
+	rsync -a --link-dest=${FC_DATADIR}/current ${FC_SOURCE} ${FC_DATADIR}/back-$date
+	fc_post_processing
+}
+
+# if directory doesnt exist, assume not starting point
+[ ! -d ${FC_DATADIR} ] && fc_create_starting_point
+
+# if current directory exists, assume a starting point is there
+[ -d ${FC_DATADIR}/current ] && fc_create_new_point
+
+
