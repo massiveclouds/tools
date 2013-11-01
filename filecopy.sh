3ad500328bdac9b02895db45672850b33e32396c
christopher mera <chris@massiveclouds.com>
2013-10-31 21:24:15 -0400

    Added error checking for script arguments

--- filecopy.sh
+++ filecopy.sh
@@ -5,6 +5,7 @@ FC_SOURCE=$1
 date=`date "+%Y-%m-%dT%H:%M:%S"`
 
 function fc_post_processing {
+	echo "* Updating 'current' link -> back-${date}"
 	rm -f ${FC_DATADIR}/current
 	ln -s back-$date ${FC_DATADIR}/current
 	exit 0;
@@ -24,10 +25,14 @@ function fc_create_new_point {
 	fc_post_processing
 }
 
-# if directory doesnt exist, assume not starting point
+# if no argument provided, do nothing
+[ -z ${FC_SOURCE} ] || echo "* No source provided" && exit 1;
+
+# if directory exists then continue otherwise exit
+[ -d ${FC_SOURCE} ]  || echo "* Source doesn't exist" && exit 1;
+
+# if 'Backups/files' directory doesnt exist, safe to assume no starting point
 [ ! -d ${FC_DATADIR} ] && fc_create_starting_point
 
-# if current directory exists, assume a starting point is there
+# if 'Backups/files/current' link exists, create a new restore point
 [ -d ${FC_DATADIR}/current ] && fc_create_new_point
-
-
