0b9e93dc671b367206a9b22ffe5fcf4588d7ae0d
christopher mera <chris@massiveclouds.com>
2013-10-31 20:25:42 -0400

    Added test for FC_DATADIR

--- filecopy.sh
+++ filecopy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 FC_ROOT="/opt/massive"
-FC_DATADIR="$FC_ROOT/Backups/files"
+FC_DATADIR="${FC_ROOT}/Backups/files"
 FC_SOURCE=$1
+
+[ -d ${FC_DATADIR} ] && echo "$FC_DATADIR found" || mkdir -p $FC_DATADIR
 
 date=`date "+%Y-%m-%dT%H:%M:%S"`
 rsync -aP --link-dest=${FC_DATADIR}/current ${FC_SOURCE} ${FC_DATADIR}/back-$date
