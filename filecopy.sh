0fa6508576e0c3dcd0864d70d3abb7100adce6a2
Christopher Mera <chris@massiveclouds.com>
2013-11-06 05:54:31 +0000

    Removed function keyword and made it sh/bash friendly, updated exception logic too

--- filecopy.sh
+++ filecopy.sh
@@ -10,14 +10,14 @@ FC_DATADIR="${FC_ROOT}/Backups/files"
 FC_SOURCE=$1
 date=`date "+%Y-%m-%dT%H:%M:%S"`
 
-function fc_post_processing {
+fc_post_processing () {
 	echo "* Updating 'current' link -> back-${date}"
 	rm -f ${FC_DATADIR}/current
 	ln -s back-$date ${FC_DATADIR}/current
 	exit 0;
 }
 
-function fc_create_starting_point {
+fc_create_starting_point () {
 	echo "* Creating $FC_DATADIR "
 	mkdir -p ${FC_DATADIR}
 	echo "* Creating first full backup "	
@@ -25,14 +25,14 @@ function fc_create_starting_point {
 	fc_post_processing
 }
 
-function fc_create_new_point {
+fc_create_new_point () {
 	echo "* Creating new restore point"
 	rsync -a --link-dest=${FC_DATADIR}/current ${FC_SOURCE} ${FC_DATADIR}/back-$date
 	fc_post_processing
 }
 
 # if no argument provided, do nothing
-[ -z ${FC_SOURCE} ] || echo "* No source provided" && exit 1;
+[ -z ${FC_SOURCE} ] &&  echo "* No source provided" && exit 1;
 
 # if directory exists then continue otherwise exit
 [ -d ${FC_SOURCE} ]  || echo "* Source doesn't exist" && exit 1;
