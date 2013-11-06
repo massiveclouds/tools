3e3aa97667f79e339cf2d4cd8e173c15c9de79cb
Christopher Mera <chris@massiveclouds.com>
2013-11-06 06:43:54 +0000

    Removed exclusion list from cloudcopy.sh

--- cloudcopy.sh
+++ cloudcopy.sh
@@ -3,6 +3,8 @@
 # Written by Christopher Mera
 # chris@massiveclouds.com
 
+CC_SOURCE="/opt/massive/Backups/files/current"
+
 # Parameters for authentication to the Storage Repository
 export AWS_ACCESS_KEY_ID="***REMOVED***"
 export AWS_SECRET_ACCESS_KEY="***REMOVED***"
@@ -19,22 +21,10 @@ KEEP_FULLS="2W"
 BUCKET_NAME='***REMOVED***'
 YEAR=$(date +%Y)
 #SRC="/tmp/source"
-SRC="/var/www/httpdocs"
-EXCLIST=( "/var/www/httpdocs/media/catalog/product" \
-	 "/var/www/httpdocs/var/backup/" \
-	 "/var/www/httpdocs/var/cache/" \
-	"/var/www/httpdocs/var/session/" \
-	)
 ###### DO NOT EDIT BELOW THIS LINE #######
 ###### DO NOT EDIT BELOW THIS LINE #######
 ###### DO NOT EDIT BELOW THIS LINE #######
 ###### DO NOT EDIT BELOW THIS LINE #######
-
-for exclude in ${EXCLIST[@]}
-	do
-	TMP=" --exclude "$exclude
-	EXCLUDE=$EXCLUDE$TMP
-done
 
 # Breakup the Volumes to 200MB chunks, upload like crazy. Keep requests low.
 PARAMS="--no-encryption --volsize 200 -vinfo --asynchronous-upload --full-if-older-than ${FULL_INTERVAL}"
@@ -46,13 +36,13 @@ function fullbackup()
 {
 	# Simple test to make sure we arent running this more than once.
 	[ `ps axu | grep -v "grep" | grep --count "duplicity"` -gt 0 ] && exit 1
-	duplicity $PARAMS $EXCLUDE $SRC $DEST
+	duplicity $PARAMS $SRC $DEST
 }
 
 # Restore data and shit
 function usage()
 {
-	echo "Whoa and shit"
+	echo "There was an unspecified error."
 }
 
 function restorelast() 
