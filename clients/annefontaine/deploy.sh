8f1c20bf8771edb1d82d8b0a907cbf3e1d2c29f5
christopher mera <camusny@gmail.com>
2014-10-03 03:43:31 +0000

    notify users via email added

--- clients/annefontaine/deploy.sh
+++ clients/annefontaine/deploy.sh
@@ -4,16 +4,29 @@
 #
 
 # set up variables / configuration
+ADMIN_EMAILS="bmerine@manhatech.com,chris@massiveclouds.com"
+HEADER="From: Cloud Deploy Bot <no-reply@***REMOVED***>"
 CODEPATH="/home/developer/current"
 DOMAIN="***REMOVED***"
 CURRENTCODE="/var/www/${DOMAIN}/public_html"
 REVISION=$(cat ${CODEPATH}/.revision)
 DOCROOT="/var/www/${DOMAIN}/current${REVISION}"
 DATE=$(date +%m%d%Y)
+ERRORMSG=""
+LOGFILE="/tmp/deploy-rsync-$DATE-$REVISION.log"
 
 # pretty print
 function pprint() {
 	echo \<=== $*
+}
+
+# copy new existing codebase to /var/www/$DOMAIN/current$REVISION
+# then rsync with new uploaded version
+function magento_copy_current() {
+	pprint "Performing upgrade to codebase with new revision"
+	pprint "Logging file updates to /tmp/deploy-rsync-$DATE-$REVISION.log"
+	cp -Lrp $CURRENTCODE $DOCROOT
+	rsync -avpz $CODEPATH/ $DOCROOT 2>&1 >> $LOGFILE
 }
 
 # compress js files
@@ -27,32 +40,27 @@ function magento_compress_js() {
 # remove cache
 function magento_remove_cache() {
 	pprint "Removing cache from $CURRENTCODE"
- rm -rf $CURRENTCODE/var/session $CURRENTCODE/var/cache $CURRENTCODE/var/full_page_cache
+ 	rm -rf $CURRENTCODE/var/cache $CURRENTCODE/var/full_page_cache
 }
-
-# copy new existing codebase to /var/www/$DOMAIN/current$REVISION
-# then rsync with new uploaded version
-function magento_copy_current() {
-	pprint "Performing upgrade to codebase with new revision"
-	pprint "Logging file updates to /tmp/deploy-rsync-$DATE-$REVISION.log"
- cp -Lrp $CURRENTCODE $DOCROOT
- rsync -avpz $CODEPATH/ $DOCROOT 2>&1 >> /tmp/deploy-rsync-$DATE-$REVISION.log
-}
-
 
 # update symbolic link for nginx configuration
 function update_link() {
 	pprint "Updating symlink for nginx for $DOCROOT"
- rm -rf $CURRENTCODE
- ln -sf $DOCROOT $CURRENTCODE
+	rm -rf $CURRENTCODE
+	ln -sf $DOCROOT $CURRENTCODE
 }
 
 function flush_php() {
 # restart php5-fpm
 	pprint "Flushing php5-fpm apc cache"
- sudo service php5-fpm restart
+	sudo service php5-fpm restart
 }
 
+function notify_users() {
+# notify users when processing is completed
+	pprint "Emailing ${ADMIN_EMAILS}"
+	printf "$*" | mail -a "$HEADER" -s "Deployment of Revision ${REVISION} Status (automatic-deploy)" ${ADMIN_EMAILS}  
+}
 
 # main logic
 curuser=$(whoami)
@@ -67,6 +75,11 @@ else
 	magento_remove_cache
 	update_link
 	flush_php
+	notify_users "This deployment was successful, $currev was updated to $REVISION.\nLog Output:\n$(/bin/cat $LOGFILE)"
+	exit
+	else
+	ERRORMSG="The current revision of ${currev} matches the code found in ${CODEPATH}"
 	fi
 fi
-pprint "Deploy script exited"
+pprint "Deploy script exited due to an error, and nothing was deployed."
+notify_users "Deploy exited due to an error, and nothing was deployed. \n${ERRORMSG}"
