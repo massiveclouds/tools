eb21e218da1db2b03917883a08505e9460556845
christopher mera <chris@massiveclouds.com>
2014-10-18 12:20:06 -0400

    rsync_copy.sh only creates a current point in time backup

--- bootstrap-sync/rsync_copy.sh
+++ bootstrap-sync/rsync_copy.sh
@@ -6,35 +6,27 @@
 # TODO: Exclusion lists
 # TODO: Move configuration variables to external config file, /opt/massive/etc/?
 source /etc/massive/sync.conf
-FC=`basename $0`
 date=`date "+%Y-%m-%dT%H:%M:%S"`
 
-fc_post_processing () {
+fc_update_link () {
 	echo "* Updating 'current' link -> back-${date}"
 	rm -f ${FC_DATADIR}/current
 	ln -s back-$date ${FC_DATADIR}/current
 	exit 0;
 }
 
-fc_create_starting_point () {
-	echo "* Creating $FC_DATADIR "
+fc_create_current_point () {
+	echo "* Preparing to add data to $FC_DATADIR "
 	mkdir -p ${FC_DATADIR}
-	echo "* Creating first full backup "	
+	echo "* Performing backup saved to $FC_DATADIR"	
 	rsync -a ${FC_SOURCE} ${FC_DATADIR}/back-$date
-	fc_post_processing
+	fc_update_link
 }
 
-fc_create_new_point () {
-	echo "* Creating new restore point"
-	rsync -a --link-dest=${FC_DATADIR}/current ${FC_SOURCE} ${FC_DATADIR}/back-$date
-	fc_post_processing
-}
-
-usage() {
-	printf "Invalid arguments!\n"
-	printf "\t$FC: <directory to backup>"
-	printf "\n"
-	exit;
+fc_delete_current_point () {
+	echo "* Removing stale data"
+	rm -rf ${FC_DATADIR}
+	fc_create_current_point
 }
 
 # if no argument provided, do nothing
@@ -44,7 +36,7 @@ usage() {
 [ -d ${FC_SOURCE} ]  || echo "Source Directory doesn't exist"
 
 # if 'Backups/files' directory doesnt exist, safe to assume no starting point
-[ -d ${FC_DATADIR} ] ||  fc_create_starting_point
+[ -d ${FC_DATADIR} ] ||  fc_create_current_point
 
 # if 'Backups/files/current' link exists, create a new restore point
-[ -d ${FC_DATADIR}/current ] && fc_create_new_point
+[ -d ${FC_DATADIR}/current ] && fc_delete_current_point
